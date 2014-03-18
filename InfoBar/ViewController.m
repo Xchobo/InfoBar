c0e1ecc49832a514f1ee8da06c788f2830c0ac06
Xchobo <xchobo@gmail.com>
2014-03-19 02:33:28 +0800

    InfoBar

--- InfoBar/ViewController.m
+++ InfoBar/ViewController.m
@@ -7,12 +7,21 @@
 //
 
 #import "ViewController.h"
+#import "ZAActivityBar.h"
+
 
 @interface ViewController ()
+
+- (IBAction)show:(id)sender;
+- (IBAction)showSuccess:(id)sender;
+- (IBAction)showError:(id)sender;
+- (IBAction)dismiss:(id)sender;
+
 
 @end
 
 @implementation ViewController
+
 
 - (void)viewDidLoad
 {
@@ -26,4 +35,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)show:(id)sender {
+    // 呼叫訊息
+    [ZAActivityBar showWithStatus:@"目前網路異常中..."];
+}
+
+- (IBAction)showSuccess:(id)sender {
+    // 通知訊息成功
+    [ZAActivityBar showSuccessWithStatus:@"成功連結至網路"];
+}
+
+- (IBAction)showError:(id)sender {
+    // 出現錯誤後隱藏
+    [ZAActivityBar showErrorWithStatus:@"發生無法處理的錯誤"];
+}
+
+- (IBAction)dismiss:(id)sender {
+    // 強制關閉訊息
+    [ZAActivityBar dismiss];
+}
 @end
